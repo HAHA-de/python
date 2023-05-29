2ecb6134855f13620370b604c2a451a72497cfba
HAHA-de <123972776+HAHA-de@users.noreply.github.com>
2023-05-29 16:17:46 +0000

    u4u

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ l= input('enter with a comma and space between')
 
 if i == '1':
     print(mode.mode(l))
-elif i=='3':
+elif i == '3':
     print(range.a(l))
 else :
     print(medienfinder.medein(l))
